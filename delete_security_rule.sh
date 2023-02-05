886523fdfbb4ae5aed3a910c3bfd82ada7301a5c
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 23:32:45 +0530

    Update delete_security_rule.sh

--- delete_security_rule.sh
+++ delete_security_rule.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Example script to delete an IBM Cloud security group rule
-sleep 120
-# Get the name of the security rule to be deleted
-rule_name="$ibm_is_security_group_rule.inbound_tcp_port_22"
+sleep 60
+resource_group_name="ibm_resource_group"
+security_rule_name="ibm_is_security_group_rule.inbound_tcp_port_22"
 
 # Delete the security rule using the IBM Cloud CLI
-ibmcloud target -r eu-de
-ibmcloud is security-group-rule-delete "$rule_name" --force 
+terraform destroy -target=ibm_is_security_group_rule.rule -var "resource_group_name=$resource_group_name" -var "security_rule_name=$security_rule_name"
 
 # Check the result of the delete command
 if [ $? -eq 0 ]; then
