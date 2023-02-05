ac4536b92b39f020931c064d4dca4aa7acd11dce
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 22:32:51 +0530

    Create delete_security_rule.sh

---
+++ delete_security_rule.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Example script to delete an IBM Cloud security group rule
+
+# Get the name of the security rule to be deleted
+rule_name=$1
+
+# Delete the security rule using the IBM Cloud CLI
+ibmcloud is security-group-rule-delete "$rule_name" --force
+
+# Check the result of the delete command
+if [ $? -eq 0 ]; then
+  echo "Successfully deleted security rule $rule_name"
+else
+  echo "Failed to delete security rule $rule_name"
+fi
