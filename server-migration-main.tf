5c6b7ddbdf06a63cc3d9b8b04149651c68e5cb2c
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 22:13:01 +0530

    Update server-migration-main.tf

--- server-migration-main.tf
+++ server-migration-main.tf
@@ -10,10 +10,6 @@ data "ibm_is_subnet" "subnet" {
   name = var.subnet_name
 }
 
-data "ibm_is_ssh_key" "ssh_key_id" {
-  name = var.ssh_key
-}
-
 resource "ibm_is_security_group" "sg" {
   name           = "${var.host_name}-sg"
   vpc            = data.ibm_is_vpc.vpc.id
@@ -48,36 +44,4 @@ resource "ibm_is_security_group_rule" "outbound_all" {
   remote    = "0.0.0.0/0"
 }
 
-#resource "ibm_is_image" "custom_image" {
-#  name             = "${var.host_name}-cent-os-7"
-#  href             = var.image_url
-#  operating_system = "centos-7-amd64"
-#  resource_group   = data.ibm_resource_group.rg.id
-#  timeouts {
-#    create = "90m"
-#    delete = "90m"
-#  }
-#}
 
-resource "ibm_is_instance" "vsi" {
-  name           = "${var.host_name}"
-  vpc            = data.ibm_is_vpc.vpc.id
-  zone           = var.zone
-  keys           = [data.ibm_is_ssh_key.ssh_key_id.id]
-  resource_group = data.ibm_resource_group.rg.id
-  image          = var.image
-  profile        = var.profile
-
-  user_data = file("download_script.sh")
-  primary_network_interface {
-    subnet          = data.ibm_is_subnet.subnet.id
-    security_groups = [ibm_is_security_group.sg.id]
-  }
-}
-
-resource "ibm_is_floating_ip" "fip" {
-  count          = var.attach_floating_ip ? 1 : 0
-  name           = "${var.host_name}-fip"
-  target         = ibm_is_instance.vsi.primary_network_interface[0].id
-  resource_group = data.ibm_resource_group.rg.id
-}
