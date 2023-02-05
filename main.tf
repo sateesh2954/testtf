c95cb0a6e7f2d6f8448780a230f1af107d49e004
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 21:34:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-resource "ibm_is_security_group" "sg" {
-  name        = "my-security-group"
-  vpc         = var.vpc_name
+resource "ibm_is_security_group" "login_sg" {
+  name           = "test-sat-sg"
+  vpc            = data.ibm_is_vpc.vpc.id
+  resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_security_group_rule" "sg_rule" {
-  security_group = ibm_is_security_group.sg.id
-  direction      = "ingress"
-  remote         = "0.0.0.0/0"
-  protocol       = "tcp"
-  port_min       = 22
-  port_max       = 22
+resource "ibm_is_security_group_rule" "login_ingress_tcp" {
+  group     = ibm_is_security_group.login_sg.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
 }
