21994ce586a663dbf22c96c33382b13ff73e1720
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 21:49:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+data "ibm_resource_group" "rg" {
+  name = var.resource_group
+}
+
+data "ibm_is_region" "region" {
+  #name = join("-", slice(split("-", var.zone), 0, 2))
+  name = var.region_name
+}
+
+data "ibm_is_zone" "zone" {
+  name   = var.zone
+  region = data.ibm_is_region.region.name
+}
+
+data "ibm_is_vpc" "existing_vpc" {
+  // Lookup for this VPC resource only if var.vpc_name is not empty
+  count = var.vpc_name != "" ? 1:0
+  name = var.vpc_name
+}
+
+data "ibm_is_vpc" "vpc" {
+  name = local.vpc_name
+  // Depends on creation of new VPC or look up of existing VPC based on value of var.vpc_name,
+  depends_on = [ibm_is_vpc.vpc, data.ibm_is_vpc.existing_vpc]
+}
+
 data "template_file" "primary_user_data" {
   template = local.primary_template_file
   vars = {
