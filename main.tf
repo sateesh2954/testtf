75b17dc46261681b4c48d9d98c2fbb55704a9b8c
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-05 21:42:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "template_file" "primary_user_data" {
+  template = local.primary_template_file
+  vars = {
+    vpc_apikey_value     = var.api_key
+    region_name          = data.ibm_is_region.region.name
+    zone_name            = data.ibm_is_zone.zone.name
+    vpc_id               = data.ibm_is_vpc.vpc.id
+  }
+}
+
 resource "ibm_is_security_group" "login_sg" {
   name           = "test-sat-sg"
   vpc            = data.ibm_is_vpc.vpc.id
