ccf3b68cb88c818b3d83306d925298ae19e2403e
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-08 00:51:09 +0530

    Update delete_security_rule.sh

--- delete_security_rule.sh
+++ delete_security_rule.sh
@@ -12,12 +12,12 @@ security_group_id=$(ibmcloud is security-groups --output json | jq --raw-output 
 echo "SG ID is $security_group_id"
 
 # Fetch security rule ID using IBM Cloud CLI
-security_rule_id=$(ibmcloud is security-group-rules --group-id $security_group_id | awk 'NR==2{print $1}')
+security_rule_id=$(ibmcloud is security-group-rules $security_group_name --vpc sat-test | awk 'NR==4{print $1}')
 
 echo "SG rule ID is $security_rule_id"
 
 # Delete security rule using IBM Cloud CLI
-ibmcloud is security-group-rule-delete $security_rule_id -g $security_group_id
+ibmcloud is security-group-rule-delete $security_group_name --vpc sat-test $security_rule_id 
 
 if [ $? -eq 0 ]; then
   echo "Security rule successfully deleted"
