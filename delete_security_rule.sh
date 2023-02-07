8bfc1a8c3e13896bfab85aec16d9cd16a9cd9a60
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-08 00:09:39 +0530

    Update delete_security_rule.sh

--- delete_security_rule.sh
+++ delete_security_rule.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Example script to delete an IBM Cloud security group rule
-sleep 60
+# Replace SECURITY_GROUP_NAME with the actual value
 
-# Retrieve the security rule ID from the Terraform state file
-rule_id=$(terraform output security_group_id)
-echo $rule_id
-# Use the IBM Cloud CLI to delete the security rule
 ibmcloud target -r eu-de
+ibmcloud target -g HPCC
+security_group_name="test-sg2"
 
-ibmcloud is security-group-rule-delete $rule_id
+# Fetch security group ID using IBM Cloud CLI
+security_group_id=$(ibmcloud is security-groups --output json | jq --raw-output '.[] | select(.name == "'"$security_group_name"'") | .id')
 
-# Check the result of the delete command
+# Fetch security rule ID using IBM Cloud CLI
+security_rule_id=$(ibmcloud is security-group-rules --group-id $security_group_id | awk 'NR==2{print $1}')
+
+# Delete security rule using IBM Cloud CLI
+ibmcloud is security-group-rule-delete $security_rule_id -g $security_group_id
+
 if [ $? -eq 0 ]; then
-  echo "Successfully deleted security rule $rule_id"
+  echo "Security rule successfully deleted"
 else
-  echo "Failed to delete security rule $rule_id"
+  echo "Error deleting security rule"
 fi
