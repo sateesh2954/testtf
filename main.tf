bbfe4247b40d632db03853aa135f1128fca78b86
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2023-02-14 21:53:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "null_resource" "delete_schematics_ingress_security_rule" { # This code
   provisioner "local-exec" {
     environment = {
       REFRESH_TOKEN       = data.ibm_iam_auth_token.token.iam_refresh_token
-      REGION              = var.region_name
+      REGION              = var.ibm_region
       SECURITY_GROUP      = ibm_is_security_group.sg.id
       SECURITY_GROUP_RULE = ibm_is_security_group_rule.inbound_tcp_port_22.id
     }
